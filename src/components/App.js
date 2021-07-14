71bb81e8dc0ac47024347e1f85130f103001f3d8
Stephen Crane <scrane88@gmail.com>
2021-07-14 16:16:35 -0400

    core deliverables done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [eatenSushis, setEatenSushis] = useState([])
+  const [money, setMoney] = useState(40)
+
+  useEffect(() => {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(data => setSushis(data))
+  }, [])
+
+  const handleEatSushi = (id, price) => {
+    if (money >= price) {
+      setEatenSushis([...eatenSushis, id])
+      console.log(eatenSushis)
+      setMoney(money => money - price)
+    } else {
+      alert("You can't afford that sushi!")
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+        sushis={sushis}
+        eatenSushis={eatenSushis}
+        onEatSushi={handleEatSushi}
+      />
+      <Table 
+        plates={eatenSushis}
+        money={money}
+      />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, eatenSushis, onEatSushi }) {
+  const [conveyorPosition, setConveyorPosition] = useState(0)
+
+  const sushiComponents = sushis.map(sushi => {
+    return (
+      <Sushi
+        key={sushi.id}
+        id={sushi.id}
+        name={sushi.name}
+        image={sushi.img_url}
+        price={sushi.price}
+        eaten={eatenSushis.includes(sushi.id)}
+        onEatSushi={onEatSushi}
+      />
+    )
+  })
+
+  console.log(sushis)
+
+  const sushiToDisplay = () => {
+    if ((conveyorPosition + 4) > 100) {
+      
+    } else {
+      return sushiComponents.slice(conveyorPosition, conveyorPosition + 4)
+    }
+  }
+
+  const handleMoreSushi = () => {
+    setConveyorPosition(conveyorPosition => conveyorPosition + 4)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiToDisplay()}
+      <MoreButton onMoreSushi={handleMoreSushi}/>
     </div>
   );
 }
